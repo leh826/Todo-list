e00c6d9e85767f1b493346a8295116871834732e
Leh826 <dev.rocha826@gmail.com>
2024-11-28 00:53:18 -0300

    add: configurações para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/Todo-list",
   server: {
     host: '0.0.0.0', 
     port: 5000,
-    base:"/Todo-list",
+  },build: {
+    outDir: 'dist',
   },
 });
